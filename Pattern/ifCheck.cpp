fcb557bb63aab42d4783b87442f9e90ef04532df
Surk7301 <sukr7581@gmail.com>
2025-06-21 13:17:13 +0530

    pattern

--- Pattern/ifCheck.cpp
+++ Pattern/ifCheck.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 
 int score = 350;
+int balance;
 int marks;
-int main(){
+int sides;
+int main()
+{
     // if(score>300){
     //     cout << "India wins the match!" << endl;
 
@@ -12,19 +15,44 @@ int main(){
 
     // }
 
-    cout << "Enter your marks: ";
-    cin >> marks;
-    if(marks >= 90){
-        cout << "Grade A" << endl;
-    }else if(marks >= 80){
-        cout << "Grade B" << endl;
-    }else if(marks >= 70){
-        cout << "Grade C" << endl;
-    }else if(marks >= 60){
-        cout << "Grade D" << endl;
-    }else if(marks >= 50){
-        cout << "Grade E" << endl;
-    }else{
-        cout << "Grade F" << endl;
+    // cout << "Enter your marks: ";
+    // cin >> marks;
+    // if(marks >= 90){
+    //     cout << "Grade A" << endl;
+    // }else if(marks >= 80){
+    //     cout << "Grade B" << endl;
+    // }else if(marks >= 70){
+    //     cout << "Grade C" << endl;
+    // }else if(marks >= 60){
+    //     cout << "Grade D" << endl;
+    // }else if(marks >= 50){
+    //     cout << "Grade E" << endl;
+    // }else{
+    //     cout << "Grade F" << endl;
+    // }
+
+    // Maggie code
+    // cout << "Enter the balance: ";
+    // cin >> balance;
+    // if (balance >= 10)
+    // {
+    //     cout << "Maggi";
+    // }
+    // else
+    // {
+    //     cout << "Kurkure";
+    // }
+
+    // triangle status
+    cout << "Enter the no. of sides: ";
+    cin >> sides;
+
+    if (sides == 3)
+    {
+        cout << "It's a Triangle";
+    }
+    else
+    {
+        cout << "Not a Triangle";
     }
 }
