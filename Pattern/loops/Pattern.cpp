8a49015deb27c711979c64a11e0b57a29228e721
Suraj Kumar <sukr7581@gmail.com>
2025-06-23 01:59:19 +0530

    Hollow square and half pyramid pattern

--- Pattern/loops/Pattern.cpp
+++ Pattern/loops/Pattern.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int n = 4;
+    int n = 5;
 
     // Square Pattern(n);
     // for (int i = 1; i <= n; i++)
@@ -17,12 +17,36 @@ int main()
     // }
 
     // Rectangle Pattern(n);
-    for (int i = 1; i <= 3; i++)
-    {
+    // for (int i = 1; i <= 3; i++)
+    // {
 
-        for (int j = 0; j <= n; j++)
-        {
-            cout << " * ";
+    //     for (int j = 0; j <= n; j++)
+    //     {
+    //         cout << " * ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // Hollow Square Pattern(n);
+    // for(int i=0; i<5; i++){
+        
+    //     for(int j=0; j<5; j++){
+    //         if(i==0 || i==4){
+    //             cout << "* ";
+    //         }else{
+    //             if(j==0 || j==4){
+    //                 cout << "* ";
+    //             }else{
+    //                 cout << "  ";
+    //             }
+    //         }
+    //     }
+    //     cout<< endl;
+    // }
+
+    for(int row =0; row<5; row++){
+        for(int col=0; col<row+1; col++){
+            cout << "* ";
         }
         cout << endl;
     }
